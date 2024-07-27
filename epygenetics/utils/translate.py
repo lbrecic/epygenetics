2da25938271edce85b6a231db6267f92e4b49523
Luka Brecic <luka.brecic3@gmail.com>
2024-07-27 19:13:51 +0200

    chore: update translate.py functionality

--- epygenetics/utils/translate.py
+++ epygenetics/utils/translate.py
@@ -1,11 +1,28 @@
+import os.path
+
 import pandas as pd
 import pyreadr
 
-name = 'BMI_CpGs'
-cpgs_dict = pyreadr.read_r(f'../../CpGs_data/{name}.rda')
-cpgs = pd.DataFrame(cpgs_dict[name])
-#print(cpgs_dict)
-print(cpgs.index)
-print(cpgs)
+cpgs_names = ['Alcohol_CpGs', 'BMI_CpGs', 'Bocklandt_CpG', 'Bohlin_CpGs', 'DNAmClockCortical_CpGs', 'DNAmTL_CpGs','EpiToc2_CpGs', 'EpiToc_CpGs', 'Garagnani_CpG', 'Hannum_CpGs','Horvath1_CpGs', 'Horvath2_CpGs', 'HorvathOnlineRef','HRSInCHPhenoAge_CpGs', 'hypoClock_CpGs', 'Knight_CpGs','LeeControl_CpGs', 'LeeRefinedRobust_CpGs', 'LeeRobust_CpGs','Lin_CpGs', 'Mayne_CpGs', 'MiAge_CpGs', 'PEDBE_CpGs','PhenoAge_CpGs', 'Smoking_CpGs', 'VidalBralo_CpGs','Weidner_CpGs', 'Zhang2019_CpGs', 'Zhang_10_CpG']
+imputes_names = ['DNAmClockCortical_imputeRef', 'Mayne_impute']
+example_names = ['exampleBetas', 'examplePheno', 'HorvathOnlineRef']
+parameters_names = ['MiAge_parameters']
 
+cpgs_output_path = '../CpGs/'
+imputes_output_path = '../imputes/'
+examples_output_path = '../examples/'
+params_output_path = '../params/'
+
+files = [(cpgs_names, cpgs_output_path), (imputes_names, imputes_output_path), (example_names, examples_output_path), (parameters_names, params_output_path)]
+
+for names, output_path in files:
+    if os.path.exists(output_path) == False:
+        os.mkdir(output_path)
+
+    for name in names:
+        cpgs_dict = pyreadr.read_r(f'../../RDA_data/params/{name}.rda')
+        print(cpgs_dict)
+        cpgs = pd.DataFrame(cpgs_dict[name])
+        cpgs.to_csv(f'{output_path}{name}.csv', index=False)
+        print(f'{name} done')
 
