e264b7ad2d2c4f6ca4ceeaeab8763965d2d5a15b
Luka Brecic <luka.brecic3@gmail.com>
2024-07-27 19:15:11 +0200

    feat: implement non blood clocks

---
+++ epygenetics/clocks/non_blood_clocks/dna_m_age_cortical.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+from epygenetics.clocks.regression_clock import RegressionClock
+from epygenetics.utils.anti_trafo import anti_trafo
+
+
+class DNAmAgeCorticalClock(RegressionClock):
+    def __init__(self):
+        cpgs = pd.read_csv('../../CpGs/DNAmClockCortical_CpGs.csv')
+        super().__init__('DNAmClockCortical', cpgs, 'CpGs', 'coef', 0.577682570446177)
+
+    def calculate(self, common_cpgs, cpg_check, dna_m, pheno, imputation):
+        if cpg_check or imputation:
+            beta_values = dna_m[common_cpgs]
+            coefficients = self.cpgs.set_index(self.marker_name).loc[common_cpgs, self.coef_name]
+            tt = np.dot(beta_values, coefficients) + self.reg_coef
+            dna_m_age_cortical = anti_trafo(tt)
+
+            if pheno is not None:
+                pheno[self.name] = dna_m_age_cortical
+                return pheno
+            else:
+                return pd.Series(dna_m_age_cortical, index=dna_m.index)
+
+        else:
+            raise Exception("CpG Check failed and imputation is not enabled or feasible.")
