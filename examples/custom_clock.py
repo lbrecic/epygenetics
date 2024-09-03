593c773dd899b0cfc122fc2cfbf5a3cfb92d8528
Luka Brecic <luka.brecic3@gmail.com>
2024-09-04 01:00:28 +0200

    chore: refactor example clocks

--- examples/custom_clock.py
+++ examples/custom_clock.py
@@ -1,22 +1,65 @@
 from typing import Optional, Tuple, Union
-
 import numpy as np
 import pandas as pd
-
 from epygenetics.clocks.base_clocks.clock import Clock
 from epygenetics.imputers import ImputerType
+from epygenetics.imputers.base_imputer import BaseImputer
+from epygenetics.imputers.factory import ImputerFactory
+from epygenetics.utils.anti_trafo import anti_trafo
+from epygenetics.utils.trafo import trafo
 
 
 class CustomClock(Clock):
 
-    def __init__(self) -> None:
-        cpgs: Optional[pd.DataFrame] = pd.read_csv('../data/examples/exampleCpGs.csv')
-        super().__init__("CustomRegressionClock", 'CpG', 'Coef', 0.333, cpgs)
+    def __init__(self, reg_coef: float = 50.412, coef_name: str = 'Coef', cpgs: Optional[pd.DataFrame] = None) -> None:
+        self.reg_coef: float = reg_coef
+        self.coef_name: str = coef_name
+        super().__init__("CustomClock", 'CpG', cpgs)
 
-    def validate(self, dna_m: pd.DataFrame, is_imputation: bool = False, imputer_type=ImputerType.REGULAR,
-                 cpg_imputation: Optional[pd.DataFrame] = None) -> Tuple[np.ndarray, bool]:
-        pass
+    def validate(self,
+                 dna_m: pd.DataFrame,
+                 is_imputation: bool = False,
+                 imputer_type=ImputerType.REGULAR,
+                 cpg_imputation: Optional[pd.DataFrame] = None
+                 ) -> Tuple[np.ndarray, bool]:
+        if self.cpgs is None:
+            raise ValueError("CpGs not loaded.")
 
-    def calculate(self, dna_m: pd.DataFrame, common_cpgs: np.ndarray, cpg_check: bool, pheno: Optional[pd.DataFrame],
-                  is_imputation: bool) -> Union[pd.DataFrame, pd.Series]:
-        pass
+        present_cpgs: np.ndarray = np.intersect1d(self.cpgs[self.marker_name], dna_m.columns)
+        cpg_check: bool = len(self.cpgs[self.marker_name]) == len(present_cpgs)
+
+        if not cpg_check and is_imputation:
+            # Impute missing CpG values
+            print(f"Imputation of missing CpG Values occurred for {self.name}")
+            for cpg in self.cpgs[self.marker_name]:
+                if cpg not in dna_m.columns:
+                    dna_m[cpg] = np.nan
+
+            imputer: BaseImputer = ImputerFactory.create_imputer(imputer_type, cpg_imputation)
+            imputer.impute(dna_m)
+
+            present_cpgs = self.cpgs[self.marker_name].values
+
+        return present_cpgs, cpg_check
+
+    def calculate(self,
+                  dna_m: pd.DataFrame,
+                  common_cpgs: np.ndarray,
+                  cpg_check: bool,
+                  pheno: Optional[pd.DataFrame],
+                  is_imputation: bool
+                  ) -> Union[pd.DataFrame, pd.Series]:
+        if cpg_check or is_imputation:
+            beta_values: pd.DataFrame = dna_m[common_cpgs]
+            coefficients: pd.Series = self.cpgs.set_index(self.marker_name).loc[common_cpgs, self.coef_name]
+            tt: np.ndarray = np.dot(beta_values, coefficients) + self.reg_coef
+            tt: anti_trafo(trafo(tt))
+
+            if pheno is not None:
+                pheno[self.name] = tt
+                return pheno
+            else:
+                return pd.Series(tt, index=dna_m.index)
+
+        else:
+            raise Exception("CpG Check failed and imputation is not enabled or feasible.")
--- examples/custom_regression_clock.py
+++ examples/custom_regression_clock.py
@@ -1,11 +1,8 @@
 from typing import Optional
-
 import pandas as pd
-
 from epygenetics.clocks.base_clocks.regression_clock import RegressionClock
 
 
 class CustomRegressionClock(RegressionClock):
-    def __init__(self) -> None:
-        cpgs: Optional[pd.DataFrame] = pd.read_csv('../data/examples/exampleCpGs.csv')
+    def __init__(self, cpgs: Optional[pd.DataFrame] = None) -> None:
         super().__init__("CustomRegressionClock", 'CpG', 'Coef', 0.333, cpgs)
